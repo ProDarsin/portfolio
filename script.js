373b91afce908c1de7331fcffc773bd5975ea58b
ProDarsin <95375283+ProDarsin@users.noreply.github.com>
2023-01-06 13:13:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -28,16 +28,47 @@ const handle=(e)=>{
         document.querySelector(".required3").classList.remove("valid")
         document.querySelector(".required3").classList.add("invalid")
     }
-    else{
-        document.querySelector(".required").classList.remove("invalid")
-        document.querySelector(".required1").classList.remove("invalid")
+    else if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputAdd2.value )){
         document.querySelector(".required2").classList.remove("invalid")
-        document.querySelector(".required3").classList.remove("invalid")
-        document.querySelector(".required").classList.add("valid")
-        document.querySelector(".required1").classList.add("valid")
-        document.querySelector(".required2").classList.add("valid")
-        document.querySelector(".required3").classList.add("valid")
+        document.querySelector(".required3").classList.add("invalid")
+        document.querySelector(".required3").innerHTML = "invalid email"
+    }
+    else{
+    
+        var values = {
+            from_name: inputEmail.value,
+            email_id: inputAdd2.value,
+            subject: inputPass.value,
+            message: inputAdd.value
+        }
+        
+
+        emailjs.send("service_qj2ufwh","template_d61l555",values).then(function (res) {
+            document.querySelector(".required").classList.remove("invalid")
+            document.querySelector(".required1").classList.remove("invalid")
+            document.querySelector(".required2").classList.remove("invalid")
+            document.querySelector(".required3").classList.remove("invalid")
+            document.querySelector(".required").classList.add("valid")
+            document.querySelector(".required1").classList.add("valid")
+            document.querySelector(".required2").classList.add("valid")
+            document.querySelector(".required3").classList.add("valid")
+
+            document.querySelector(".success").innerHTML="Message sent succesfully!!"
+
+
+            document.getElementById("form").reset();
+
+            setTimeout(function(){
+                document.querySelector(".success").innerHTML = ""
+            },5000)
+        })
         
     }
 
 }
+
+
+
+//SEND EMAIL
+
+
